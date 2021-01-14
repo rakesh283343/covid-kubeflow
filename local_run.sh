33fe66fb2ce61d8d672809c468c1dbe613536eef
rawkintrevo <trevor.d.grant@gmail.com>
2021-01-14 16:19:38 -0600

    new posts

---
+++ local_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "refreshing covid data"
+cd 1-get-data/COVID-19 & git pull --ff-only
+echo "prepping data for modeling"
+python3 2-etl/prep_data.py
+echo "modeling data"
+Rscript 3z-model-using-R/model.R
+echo "writing blog posts"
+python3 4-blog-writer/writer.py
+
+echo "posting blog posts"
+cp blog_posts/* ../covid-blog/_posts
+cd ../covid-blog & git commit -am "new posts" & git push
+#rm blog_posts/*
